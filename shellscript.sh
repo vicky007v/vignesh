677963fba3453bfc8b221b8fbbb7360ed873d2d0
vicky007v <94953043+vicky007v@users.noreply.github.com>
2022-04-04 16:59:52 +0530

    Update shellscript.sh

--- shellscript.sh
+++ shellscript.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-SERVICE_NAME="newproject"
+SERVICE_NAME="newservice"
 IMAGE_VERSION="latest"${BUILD_NUMBER}
-TASK_FAMILY="newApp"
+TASK_FAMILY="newproject"
 
 # Create a new task definition for this build
 sed -e "s;latest;${BUILD_NUMBER};g" flask-signup.json > flask-signup-v_${BUILD_NUMBER}.json
-aws ecs register-task-definition --family flask-signup --cli-input-json file://flask-signup-v_${BUILD_NUMBER}.json
+aws ecs register-task-definition --newproject flask-signup --cli-input-json file://flask-signup-v_${BUILD_NUMBER}.json
 
-aws ecs update-service --cluster default --newservice ${SERVICE_NAME} --newproject ${TASK_FAMILY}:${TASK_REVISION} --1 ${DESIRED_COUNT}'
+TASK_REVISION=`aws ecs describe-task-definition --task-definition newApp | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
+
+aws ecs update-service --cluster default --newservice ${SERVICE_NAME} --newproject ${TASK_FAMILY}:${TASK_REVISION} --desired-count 1
          
